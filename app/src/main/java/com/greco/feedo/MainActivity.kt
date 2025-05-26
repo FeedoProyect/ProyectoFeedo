311ddac6c8f00d221322eaa68a1e3d93c2ddba03
Grecoo <titi18407@gmail.com>
2025-05-26 19:31:57 -0300

    Cree el inicio, y el boton para ir al menu

--- app/src/main/java/com/greco/feedo/MainActivity.kt
+++ app/src/main/java/com/greco/feedo/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.greco.feedo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.greco.feedo.menu.MenuActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            val intent=Intent(this,MenuActivity::class.java)
+            startActivity(intent)
+         }
     }
 }
