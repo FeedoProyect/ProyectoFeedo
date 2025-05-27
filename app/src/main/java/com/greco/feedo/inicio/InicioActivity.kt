8a059499363c76d8b806ed505b05834bdfce89bc
Grecoo <titi18407@gmail.com>
2025-05-27 16:06:27 -0300

    termine la interfaz de inicio

--- app/src/main/java/com/greco/feedo/inicio/InicioActivity.kt
+++ app/src/main/java/com/greco/feedo/inicio/InicioActivity.kt
@@ -1,16 +1,13 @@
-package com.greco.feedo
+package com.greco.feedo.inicio
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.greco.feedo.R
 import com.greco.feedo.menu.MenuActivity
 
-class MainActivity : AppCompatActivity() {
+class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
